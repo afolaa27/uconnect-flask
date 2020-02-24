83a238bca4cb57e982438f03da1b01857c0dd81c
Abdulkhaliq <afolaa27@uwgb.edu>
2020-02-24 14:58:55 -0600

    User can logout

--- resources/users.py
+++ resources/users.py
@@ -67,3 +67,31 @@ def login():
 			data={},
 			message='email or password incorrect',
 			status=401),401
+
+
+@users.route('loggedin', methods=['GET'])
+def get_logged_in_user():
+	if not current_user.is_authenticated:
+		return jsonify(
+			data={},
+			message='No user logged in',
+			status= 401),401
+	else :
+		user_dict = model_to_dict(current_user)
+		user_dict.pop('password')
+		user_dict['latitude']=str(user_dict['latitude'])
+		user_dict['longitude']=str(user_dict['longitude'])
+		
+		return jsonify(
+			data=user_dict,
+			message='this is the current logged in user',
+			status=200),200
+
+@users.route('logout', methods=['GET'])
+def logout():
+	logout_user()
+	return jsonify(
+		data={},
+		message="Youve been logged out",
+		status=200),200
+
