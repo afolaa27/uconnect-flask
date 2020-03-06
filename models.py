9444e9b0b15b0aada8046b6cf37e24ecba932870
Abdulkhaliq <afolaa27@uwgb.edu>
2020-03-05 22:31:17 -0600

    changed db name

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ if 'ON_HEROKU' in os.environ: # later we will manually add this env var
                                                      # when you provision the
                                                      # Heroku Postgres Add-on
 else:
-  DATABASE = SqliteDatabase('dogs.sqlite')
+  DATABASE = SqliteDatabase('uconnect.sqlite')
 
 
 class User(UserMixin, Model):
